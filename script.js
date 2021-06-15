bb12a97b62ffd5b47b7e4e1f4beed7056c047369
Amrit Thapa <amet.thapa27@gmail.com>
2021-06-15 10:27:19 +0545

    completed

--- script.js
+++ script.js
@@ -14,7 +14,7 @@ const cover = document.getElementById('cover');
 const songs = ["Life Doesn't Escape Us – Sapajou", "Maarif - Soyb & Amine Maxwell", "Middle Class (Instrumental) - RYYZN", "mn_music"  ];
 
 //keep track of songs
-let songIndex = 1;
+let songIndex = 3;
 
 //initially load song details into DOM
 loadSong(songs[songIndex]);
@@ -31,11 +31,54 @@ function playSong(){
     musicContainer.classList.add('play');
     playBtn.querySelector('i.fas').classList.remove('fa-play');
     playBtn.querySelector('i.fas').classList.add('fa-pause');
-
     audio.play();
-
 }
 
+//pausee song
+function pauseSong(){
+    musicContainer.classList.remove('play');
+    playBtn.querySelector('i.fas').classList.remove('fa-pause');
+    playBtn.querySelector('i.fas').classList.add('fa-play');
+    audio.pause();
+}
+
+function prevSong(){
+    songIndex--;
+    if(songIndex < 0){
+        songIndex = songs.length -1;
+    }
+    loadSong(songs[songIndex]);
+
+    playSong();
+}
+
+function nextSong(){
+    songIndex++;
+    if(songIndex > songs.length -1){
+        songIndex = 0;
+    }
+    loadSong(songs[songIndex]);
+
+    playSong();
+}
+
+function updateProgress(e){
+    const { duration, currentTime } = e.srcElement;
+    const progressPercent = (currentTime/duration) * 100;
+    progress.style.width = `${progressPercent}%`;
+    if(currentTime == duration){
+        nextSong();
+    }
+}
+
+//set Progress bar
+function setProgress(e){
+    const width = this.clientWidth;
+    const clickX = e.offsetX;
+    const duration = audio.duration;
+    audio.currentTime = (clickX/width)*duration;
+
+}
 
 //event listeners
 playBtn.addEventListener('click', () =>{
@@ -47,3 +90,13 @@ playBtn.addEventListener('click', () =>{
         playSong();
     }
 })
+
+//change song
+prevBtn.addEventListener('click',prevSong); 
+nextBtn.addEventListener('click',nextSong);
+
+//time/song update
+audio.addEventListener('timeupdate', updateProgress);
+
+//click om progress bar
+progressContainer.addEventListener('click', setProgress);
